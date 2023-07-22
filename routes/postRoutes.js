88866fb7f84706b8cfa37a2426adc719cbad9326
mihir sabhaya <mihirsabhaya@appscrip.co>
2023-07-22 12:22:55 +0530

    first commit

---
+++ routes/postRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+
+// Define post-related routes
+// e.g., router.post('/create', postController.createPost);
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Define user-related routes
+// e.g., router.post('/register', userController.register);
+
+module.exports = router;
